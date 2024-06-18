cbc57ba2a2df4e87e86007e6c530ac1f8c4c9bfa
danielugulino <d.ugulino@picalike.com>
2024-06-18 16:58:43 -0300

    fixes and final tests

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 docker-compose -f docker-compose.yml kill
 docker-compose -f docker-compose.yml rm -f
 docker-compose -f docker-compose.yml build
-docker-compose -f docker-compose.yml up -d
+docker-compose -f docker-compose.yml up
